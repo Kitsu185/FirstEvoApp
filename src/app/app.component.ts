c7d61ec95417d94a47dac91c1f910fbe52abe7d8
Kitsu185 <ys.salih2004@gmail.com>
2026-04-20 16:25:21 +0300

    задание 3 директивы, pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AddFivePipe } from './add-five.pipe';
 import { CounterComponent } from './counter/counter.component';
 
 @Component({
   selector: 'app-root',
-  imports: [CounterComponent],
+  standalone: true,
+  imports: [CommonModule, AddFivePipe, CounterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  hello: string = 'hello';
+  currentNumber = 10;
+  currentDate = new Date();
+
+  products = [
+    { id: 1, name: 'Laptop', price: 50000 },
+    { id: 2, name: 'Phone', price: 30000 },
+    { id: 3, name: 'Tablet', price: 25000 }
+  ];
 }
